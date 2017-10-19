450ff721a47938bfd150d92d4a24a25a7fa77af3
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-10-19 16:37:17 -0200

    Fixed spree factories

--- spec/factories/state.rb
+++ spec/factories/state.rb
@@ -2,12 +2,6 @@ FactoryGirl.modify do
   factory :state do
     name 'São Paulo'
     abbr 'SP'
-    country do |country|
-      if br = Spree::Country.find_by(iso: 'BR')
-        country = br
-      else
-        country.association(:country)
-      end
-    end
+    association :country, factory: :country
   end
 end
