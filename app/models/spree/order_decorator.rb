85abc3ff356a83105328a2e8086865f91566c2c5
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-10-22 18:24:46 -0200

    Added order default tax document

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -4,10 +4,21 @@ module Spree
   Order.class_eval do
     enum tax_document_type: %i[cpf cnpj]
 
+    before_create :set_default_tax_document
+
     validates :tax_document_type,
               :tax_document,
               presence: true
     validates :tax_document,
               cpf_or_cnpj: true
+
+    private
+
+    def set_default_tax_document
+      if user.present?
+        self.tax_document_type ||= user.tax_document_type
+        self.tax_document      ||= user.tax_document
+      end
+    end
   end
 end
