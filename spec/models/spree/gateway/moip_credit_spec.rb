4f92b81bce064b969051e876ad7cff96c8a098c3
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-10-22 04:43:32 -0200

    Added authorize_payment! spec method

--- spec/models/spree/gateway/moip_credit_spec.rb
+++ spec/models/spree/gateway/moip_credit_spec.rb
@@ -5,10 +5,15 @@ describe Spree::Gateway::MoipCredit do
   let(:gateway) { create(:moip_gateway) }
   let(:payment) { build(:payment, payment_method: gateway, order: order) }
   let(:gateway_options) { Spree::Payment::GatewayOptions.new(payment).to_hash }
+  let(:total) { payment.amount }
   let(:total_cents) { Spree::Money.new(payment.amount).cents }
   let(:source) { payment.payment_source }
   let(:transaction_id) { payment.reload.transaction_id }
   let(:add_payment_to_order!) { order.payments << payment }
+  let(:authorize_payment!) do
+    url = "/simulador/authorize?payment_id=#{transaction_id}&amount=#{total_cents}"
+    gateway.provider.client.get(url)
+  end
   let(:complete_order!) do
     add_payment_to_order!
     until order.completed? do order.next! end
@@ -31,7 +36,7 @@ describe Spree::Gateway::MoipCredit do
     before(:each) { VCR.insert_cassette 'moip_credit/purchase', record: :new_episodes }
     after(:each) { VCR.eject_cassette }
     before(:each) { add_payment_to_order! }
-    
+
     it 'succeeds' do
       expect(response.success?).to be true
     end
@@ -44,27 +49,33 @@ describe Spree::Gateway::MoipCredit do
   end
 
   xdescribe '#void' do
-    let(:response) { gateway.void transaction_id, gateway_options }
+    let(:void!) { gateway.void transaction_id, gateway_options }
     before(:each) { VCR.insert_cassette 'moip_credit/void', record: :new_episodes }
     after(:each) { VCR.eject_cassette }
-    before(:each) { complete_order! }
+    before(:each) do
+      complete_order!
+      authorize_payment!
+      void!
+    end
 
-    it 'succeeds' do
-      expect(response.success?).to be true
+    it 'updates the payment state to "void"' do
+      expect(payment.reload.state).to be 'void'
     end
   end
 
   xdescribe '#capture' do
-    let(:response) { gateway.capture total_cents, transaction_id, gateway_options }
     before(:each) { VCR.insert_cassette 'moip_credit/capture', record: :new_episodes }
     after(:each) { VCR.eject_cassette }
-    before(:each) {
+    before(:each) do
       Spree::Config[:auto_capture] = false
       complete_order!
-    }
+      authorize_payment!
+    end
 
-    it 'succeeds' do
-      expect(response.success?).to be true
+    it 'updates the payment state to "completed"' do
+      expect(payment).to be_pending
+      payment.reload.capture!
+      expect(payment).to be_completed
     end
   end
 end
