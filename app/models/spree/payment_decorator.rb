bae8087a8881a222d2f51c5375919a2730f171d2
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-10-26 06:10:18 -0200

    Fixed payment decorator actions

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -2,11 +2,13 @@ module Spree
   Payment.class_eval do
     has_one :moip_transaction
 
+    alias_method :original_actions, :actions
+    
     def actions
       if payment_method.is_a?(Spree::Gateway::MoipBase) && moip_transaction.present?
         moip_transaction.actions
       else
-        super
+        original_actions
       end
     end
   end
