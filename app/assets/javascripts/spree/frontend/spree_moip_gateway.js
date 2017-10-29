f90e466fc2537860bae26a1b642f18fad6f1ab65
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-10-29 01:22:17 -0200

    Added credit card images and js validations

--- app/assets/javascripts/spree/frontend/spree_moip_gateway.js
+++ app/assets/javascripts/spree/frontend/spree_moip_gateway.js
@@ -1,2 +1,30 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
+(function ($) {
+  if('payment' in $) {
+    $.payment.cards.unshift(
+      {
+        type: "elo",
+        luhn: true,
+        format: /^((((636368)|(438935)|(504175)|(451416)|(636297))\d{0,10})|((5067)|(4576)|(4011))\d{0,12})$/,
+        patterns: [
+          636368,438935,504175,451416,509048,509067,
+          509049,509069,509050,509074,509068,509040,
+          509045,509051,509046,509066,509047,509042,
+          509052,509043,509064,509040,36297, 5067,
+          4576,4011
+        ],
+        length: [16],
+        cvcLength: [3]
+      },
+      {
+        type: "hipercard",
+        luhn: false,
+        format: /^(606282\d{10}(\d{3})?)|(3841\d{15})$/,
+        patterns: [38,60],
+        length: [13,16,19],
+        cvcLength: [3]
+      }
+    )
+  }
+})(jQuery)
--- app/models/spree/credit_card_decorator.rb
+++ app/models/spree/credit_card_decorator.rb
@@ -2,6 +2,13 @@ require 'validates_cpf_cnpj'
 
 module Spree
   CreditCard.class_eval do
+    const_set :ORIGINAL_CARD_TYPES, CreditCard::CARD_TYPES
+    remove_const :CARD_TYPES
+    const_set :CARD_TYPES, CreditCard::ORIGINAL_CARD_TYPES.reverse_merge(
+      elo: /^((((636368)|(438935)|(504175)|(451416)|(636297))\d{0,10})|((5067)|(4576)|(4011))\d{0,12})$/,
+      hipercard: /^(606282\d{10}(\d{3})?)|(3841\d{15})$/
+    )
+
     with_options if: :moip_payment?, on: :create do
       validates :tax_document,
                 :birth_date,
