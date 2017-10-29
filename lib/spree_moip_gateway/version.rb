4eed8e045154f541c5fc9af60e6eb8c836ce978d
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-10-29 05:48:12 -0200

    v0.3.1

--- lib/spree_moip_gateway/version.rb
+++ lib/spree_moip_gateway/version.rb
@@ -10,7 +10,7 @@ module SpreeMoipGateway
   module VERSION
     MAJOR = 0
     MINOR = 3
-    TINY  = 0
+    TINY  = 1
     # PRE   = 'alpha'.freeze
 
     STRING = [MAJOR, MINOR, TINY].compact.join('.')
